41025d80fdf9b1b6c0fd1af2c3d33e67eb01f213
NarendraKumarReddyPolu <30776703+PythonAwsBoto3@users.noreply.github.com>
2017-12-23 11:46:46 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,2 @@
-print"welcome to Git and GitHub"
-print"This is for devops"
-print "This is from local repo"
-print"This is at 3.04pm"
-
-#new line
-#LAST COMMIT
+#!/bin/python
+print"welcome to jenkins integartion with github"
